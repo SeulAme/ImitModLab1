e570716c6e3d77a39454e17f7270867405c38cd0
wladik_ro@mail.ru <wladik_ro@mail.ru>
2021-03-16 15:03:56 +0300

    star

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Graph
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.GraphXYZ = new System.Windows.Forms.PictureBox();
+            this.GraphSpeed = new System.Windows.Forms.PictureBox();
+            this.Kutt = new System.Windows.Forms.Button();
+            this.Eiler = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.GraphXYZ)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.GraphSpeed)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // GraphXYZ
+            // 
+            this.GraphXYZ.BackColor = System.Drawing.Color.White;
+            this.GraphXYZ.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.GraphXYZ.Location = new System.Drawing.Point(33, 12);
+            this.GraphXYZ.Name = "GraphXYZ";
+            this.GraphXYZ.Size = new System.Drawing.Size(909, 191);
+            this.GraphXYZ.TabIndex = 0;
+            this.GraphXYZ.TabStop = false;
+            // 
+            // GraphSpeed
+            // 
+            this.GraphSpeed.BackColor = System.Drawing.Color.White;
+            this.GraphSpeed.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.GraphSpeed.Location = new System.Drawing.Point(33, 225);
+            this.GraphSpeed.Name = "GraphSpeed";
+            this.GraphSpeed.Size = new System.Drawing.Size(909, 179);
+            this.GraphSpeed.TabIndex = 1;
+            this.GraphSpeed.TabStop = false;
+            // 
+            // Kutt
+            // 
+            this.Kutt.BackColor = System.Drawing.SystemColors.Info;
+            this.Kutt.Font = new System.Drawing.Font("Arial Narrow", 7.8F, System.Drawing.FontStyle.Bold);
+            this.Kutt.Location = new System.Drawing.Point(33, 429);
+            this.Kutt.Name = "Kutt";
+            this.Kutt.Size = new System.Drawing.Size(160, 38);
+            this.Kutt.TabIndex = 2;
+            this.Kutt.Text = "Рунге-Кутт";
+            this.Kutt.UseVisualStyleBackColor = false;
+            this.Kutt.Click += new System.EventHandler(this.Kutt_Click);
+            // 
+            // Eiler
+            // 
+            this.Eiler.BackColor = System.Drawing.SystemColors.Info;
+            this.Eiler.Font = new System.Drawing.Font("Arial Narrow", 7.8F, System.Drawing.FontStyle.Bold);
+            this.Eiler.Location = new System.Drawing.Point(212, 429);
+            this.Eiler.Name = "Eiler";
+            this.Eiler.Size = new System.Drawing.Size(160, 38);
+            this.Eiler.TabIndex = 3;
+            this.Eiler.Text = "Эйлер";
+            this.Eiler.UseVisualStyleBackColor = false;
+            this.Eiler.Click += new System.EventHandler(this.Eiler_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.Info;
+            this.button1.Font = new System.Drawing.Font("Arial Narrow", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(781, 429);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(161, 38);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Очистить";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Graph
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.BackgroundImage = global::WindowsFormsApp1.Properties.Resources.image;
+            this.ClientSize = new System.Drawing.Size(987, 504);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Eiler);
+            this.Controls.Add(this.Kutt);
+            this.Controls.Add(this.GraphSpeed);
+            this.Controls.Add(this.GraphXYZ);
+            this.Name = "Graph";
+            this.Text = "Графики";
+            ((System.ComponentModel.ISupportInitialize)(this.GraphXYZ)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.GraphSpeed)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox GraphXYZ;
+        private System.Windows.Forms.PictureBox GraphSpeed;
+        private System.Windows.Forms.Button Kutt;
+        private System.Windows.Forms.Button Eiler;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace WindowsFormsApp1
+{
+    public partial class Graph : Form
+    {
+        public Graph()
+        {
+            InitializeComponent();
+        }
+        public List<TVector> PointsEiler=null;
+        public List<TVector> PointsKutt = null;
+        public class TVector
+        {
+           public double[] Vec;
+        }
+
+        public List<TVector> ReadFile(string FName) //функция чтения файла
+        {
+            List<TVector> result = null;
+            using (FileStream fstream = File.OpenRead(FName))
+            {
+                // преобразуем строку в байты
+                byte[] array = new byte[fstream.Length];
+                // считываем данные
+                fstream.Read(array, 0, array.Length);
+                // декодируем байты в строку
+                string textFromFile = System.Text.Encoding.Default.GetString(array);
+                Console.WriteLine($"Текст из файла: {textFromFile}");
+                int ItemsCount = textFromFile.Split('\n').Length;
+                result = textFromFile.Split('\n').Take(ItemsCount / 4 - 1).Select(x => new TVector()
+                {
+                    Vec = x.Split('|').Select(y => double.Parse(y.Replace('.', ','))).ToArray()
+                }).ToList();
+            }
+            return result;
+        }
+  
+private void Eiler_Click(object sender, EventArgs e)//ЭЙЛЕР
+        {
+            if (PointsEiler == null) { PointsEiler = ReadFile("result_TEuler.txt"); }
+            
+            Graphics g = GraphXYZ.CreateGraphics();
+            Graphics gr = GraphSpeed.CreateGraphics();
+            Pen myPen1 = new Pen(Color.Blue);
+            Pen myPen2 = new Pen(Color.Red);
+            Pen myPen3 = new Pen(Color.Green);
+            Pen myPen4 = new Pen(Color.Black,2);
+            double xmax = PointsEiler.Count;
+
+            double ymax = Math.Max(Math.Max(PointsEiler.Max(x=>Math.Abs(x.Vec[0])), PointsEiler.Max(x=>Math.Abs(x.Vec[1]))), PointsEiler.Max(x => Math.Abs(x.Vec[2])));
+           
+            //масштаб
+            double kx = GraphXYZ.Width / xmax; 
+            double ky = GraphXYZ.Height / ymax/2.8;
+            TVector PrevPoint = PointsEiler.FirstOrDefault();
+            PrevPoint.Vec = new double[6] { 0, 0, 0, 0, 0, 0 };
+
+            //ОСИ
+            g.DrawLine(myPen4, 0, GraphXYZ.Height / 2,GraphXYZ.Width,GraphXYZ.Height/2);
+            g.DrawLine(myPen4, 10, 10, 10, GraphXYZ.Height -10);
+
+            //КООРДИНАТЫ
+            int index = 0;
+            Font drawFont = new Font("Arial", 12);
+            SolidBrush drawBrush1 = new SolidBrush(Color.Blue);
+            g.DrawString("X", drawFont, drawBrush1, new Point(10, 10));
+            SolidBrush drawBrush2 = new SolidBrush(Color.Red);
+            g.DrawString("Y", drawFont, drawBrush2, new Point(10,30));
+            SolidBrush drawBrush3 = new SolidBrush(Color.Green);
+            g.DrawString("Z", drawFont, drawBrush3, new Point(10, 50));
+            foreach (var Point in PointsEiler)
+            {       
+                g.DrawLine(myPen1, (int)(kx *index), (int)(GraphXYZ.Height/2 + ky * PrevPoint.Vec[0]), (int)(kx * (index+1)), (int)(GraphXYZ.Height/2  + ky * Point.Vec[0]));
+                g.DrawLine(myPen2, (int)(kx * index), (int)(GraphXYZ.Height / 2 + ky * PrevPoint.Vec[1]), (int)(kx * (index + 1)), (int)(GraphXYZ.Height / 2 + ky * Point.Vec[1]));
+                g.DrawLine(myPen3, (int)(kx * index), (int)(GraphXYZ.Height / 2 + ky * PrevPoint.Vec[2]), (int)(kx * (index + 1)), (int)(GraphXYZ.Height / 2 + ky * Point.Vec[2]));
+                PrevPoint = Point;
+                index++;
+            }
+
+            //СКОРОСТИ
+            ymax = Math.Max(Math.Max(PointsEiler.Max(x => Math.Abs(x.Vec[3])), PointsEiler.Max(x => Math.Abs(x.Vec[4]))), PointsEiler.Max(x => Math.Abs(x.Vec[5])));
+            ky = GraphXYZ.Height / ymax / 2.8;
+            gr.DrawLine(myPen4, 0, GraphXYZ.Height / 2, GraphXYZ.Width, GraphXYZ.Height / 2);
+            gr.DrawLine(myPen4, 10, 10, 10, GraphXYZ.Height - 10);
+            index = 0;
+            gr.DrawString("Ux", drawFont, drawBrush1, new Point(10, 10));          
+            gr.DrawString("Uy", drawFont, drawBrush2, new Point(10, 30));           
+            gr.DrawString("Uz", drawFont, drawBrush3, new Point(10, 50));
+            foreach (var Point in PointsEiler)
+            {
+                gr.DrawLine(myPen1, (int)(kx * index), (int)(GraphXYZ.Height / 2 + ky * PrevPoint.Vec[3]), (int)(kx * (index + 1)), (int)(GraphXYZ.Height / 2 + ky * Point.Vec[3]));
+                gr.DrawLine(myPen2, (int)(kx * index), (int)(GraphXYZ.Height / 2 + ky * PrevPoint.Vec[4]), (int)(kx * (index + 1)), (int)(GraphXYZ.Height / 2 + ky * Point.Vec[4]));
+                gr.DrawLine(myPen3, (int)(kx * index), (int)(GraphXYZ.Height / 2 + ky * PrevPoint.Vec[5]), (int)(kx * (index + 1)), (int)(GraphXYZ.Height / 2 + ky * Point.Vec[5]));
+                PrevPoint = Point;
+                index++;
+            }
+
+        }
+
+        private void Kutt_Click(object sender, EventArgs e) //РУНГЕ-КУТТ
+        {
+            if (PointsKutt == null) { PointsKutt = ReadFile("result_TRungeKutta.txt"); }
+            Graphics g = GraphXYZ.CreateGraphics();
+            Graphics gr = GraphSpeed.CreateGraphics();
+            Pen myPen1 = new Pen(Color.Blue);
+            Pen myPen2 = new Pen(Color.Red);
+            Pen myPen3 = new Pen(Color.Green);
+            Pen myPen4 = new Pen(Color.Black, 2);
+            double xmax = PointsKutt.Count;
+
+            double ymax = Math.Max(Math.Max(PointsKutt.Max(x => Math.Abs(x.Vec[0])), PointsKutt.Max(x => Math.Abs(x.Vec[1]))), PointsKutt.Max(x => Math.Abs(x.Vec[2])));
+
+            //масштаб
+            double kx = GraphXYZ.Width / xmax;
+            double ky = GraphXYZ.Height / ymax / 2.8;
+            TVector PrevPoint = PointsKutt.FirstOrDefault();
+            PrevPoint.Vec = new double[6] { 0, 0, 0, 0, 0, 0 };
+            
+            //ОСИ
+            g.DrawLine(myPen4, 0, GraphXYZ.Height / 2, GraphXYZ.Width, GraphXYZ.Height / 2);
+            g.DrawLine(myPen4, 10, 10, 10, GraphXYZ.Height - 10);
+
+            //КООРДИНАТЫ
+            int index = 0;
+            Font drawFont = new Font("Arial", 12);
+            SolidBrush drawBrush1 = new SolidBrush(Color.Blue);
+            g.DrawString("X", drawFont, drawBrush1, new Point(10, 10));
+            SolidBrush drawBrush2 = new SolidBrush(Color.Red);
+            g.DrawString("Y", drawFont, drawBrush2, new Point(10, 30));
+            SolidBrush drawBrush3 = new SolidBrush(Color.Green);
+            g.DrawString("Z", drawFont, drawBrush3, new Point(10, 50));
+            foreach (var Point in PointsKutt)
+            {
+                g.DrawLine(myPen1, (int)(kx * index), (int)(GraphXYZ.Height / 2 + ky * PrevPoint.Vec[0]), (int)(kx * (index + 1)), (int)(GraphXYZ.Height / 2 + ky * Point.Vec[0]));
+                g.DrawLine(myPen2, (int)(kx * index), (int)(GraphXYZ.Height / 2 + ky * PrevPoint.Vec[1]), (int)(kx * (index + 1)), (int)(GraphXYZ.Height / 2 + ky * Point.Vec[1]));
+                g.DrawLine(myPen3, (int)(kx * index), (int)(GraphXYZ.Height / 2 + ky * PrevPoint.Vec[2]), (int)(kx * (index + 1)), (int)(GraphXYZ.Height / 2 + ky * Point.Vec[2]));
+                PrevPoint = Point;
+                index++;
+            }
+
+            //СКОРОСТИ
+            ymax = Math.Max(Math.Max(PointsKutt.Max(x => Math.Abs(x.Vec[3])), PointsKutt.Max(x => Math.Abs(x.Vec[4]))), PointsKutt.Max(x => Math.Abs(x.Vec[5])));
+            ky = GraphXYZ.Height / ymax / 2.8;
+            gr.DrawLine(myPen4, 0, GraphXYZ.Height / 2, GraphXYZ.Width, GraphXYZ.Height / 2);
+            gr.DrawLine(myPen4, 10, 10, 10, GraphXYZ.Height - 10);
+            index = 0;
+            gr.DrawString("Ux", drawFont, drawBrush1, new Point(10, 10));
+            gr.DrawString("Uy", drawFont, drawBrush2, new Point(10, 30));
+            gr.DrawString("Uz", drawFont, drawBrush3, new Point(10, 50));
+            foreach (var Point in PointsKutt)
+            {
+                gr.DrawLine(myPen1, (int)(kx * index), (int)(GraphXYZ.Height / 2 + ky * PrevPoint.Vec[3]), (int)(kx * (index + 1)), (int)(GraphXYZ.Height / 2 + ky * Point.Vec[3]));
+                gr.DrawLine(myPen2, (int)(kx * index), (int)(GraphXYZ.Height / 2 + ky * PrevPoint.Vec[4]), (int)(kx * (index + 1)), (int)(GraphXYZ.Height / 2 + ky * Point.Vec[4]));
+                gr.DrawLine(myPen3, (int)(kx * index), (int)(GraphXYZ.Height / 2 + ky * PrevPoint.Vec[5]), (int)(kx * (index + 1)), (int)(GraphXYZ.Height / 2 + ky * Point.Vec[5]));
+                PrevPoint = Point;
+                index++;
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)//ОЧИСТИТЬ
+        {
+             GraphXYZ.CreateGraphics().Clear(Color.White);
+             GraphSpeed.CreateGraphics().Clear(Color.White);
+
+        }
+
+       
+    }
+}
